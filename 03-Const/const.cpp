3a90d32d4c3332741d9424fc63be4402345591c0
Davi P. Oliveira <dpo.dev11@gmail.com>
2025-10-20 23:53:56 -0300

    added const.cpp

---
+++ 03-Const/const.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    const double PI = 3.14159;
+    const int LIGHT_SPEED = 23453244;
+    const int WIDTH = 1930;
+    const int HEIGHT = 1000;
+    //PI = 420.69; This will cause an error
+    double radius = 10;
+    double circumference = 2 * PI * radius;
+
+    std::cout << circumference << "cm";
+
+    return 0;
+}
