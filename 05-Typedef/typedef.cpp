d9bfa1cb3f54b7c94493b48676c8407c20d19e4c
Davi P. Oliveira <dpo.dev11@gmail.com>
2025-10-21 00:15:41 -0300

    added typedef

---
+++ 05-Typedef/typedef.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+//typedef std::string text_t;
+//typedef int number_t;
+using text_t = std::string;
+using number_t = int;
+
+int main(){
+
+    // typedef = reserved keyword used to create an additional name
+    //           (alias) for another data type
+    //           New identifier for an existing type 
+    //           Helps with readability and reduced typos
+    //           Use when there is a clear benefit
+    //           Replaced with 'using' (work better w/ templates)
+    
+
+    text_t firstName = "John";
+    number_t age = 31;
+
+    std::cout << firstName << '\n';
+    std::cout << age << '\n';
+
+    return 0;
+}
