7d98b25c157b5b3dca59a1dee21858327070fa71
Davi P. Oliveira <dpo.dev11@gmail.com>
2025-10-20 23:45:11 -0300

    first commit

---
+++ 01-Intro/helloworld.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    // This is a comment
+    /*
+      This 
+      is 
+      a 
+      multiline 
+      comment
+    */
+
+    std::cout << "Lorem Ipsum" << std::endl;
+    std::cout << "It's really good !" << std::endl;
+    return 0;
+}
---
+++ 02-Variables/variables.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+int main () {
+
+   int x = 5;
+   int y = 6;
+   int sum = x + y;
+
+   // Integer Whole Number
+   int age = 21;
+   int year = 2023;
+   int days = 5.6;
+
+   // double (number including decimal)
+   double price = 10.99;
+   double gpa = 2.5;
+   double temperature = 25.1;
+
+   // single character
+   char grade = 'A';
+   char initial = 'B';
+   char currency = '$';
+
+   // boolean (true or false)
+   bool student = false;
+   bool power = true;
+   bool forSale = true;
+
+   // string (objects that represent a sequence of text)
+   std::string name = "David";
+   std::string day = "Friday";
+   std::string food = "pizza";
+   std::string address = "123 Fake St.";
+
+   std::cout << "Hello " << name << '\n';
+   std::cout << "You are " << age << " years old";
+
+
+   return 0;   
+}
