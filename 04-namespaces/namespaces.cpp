c958079294733a6d0aa2ccaef94c139ecb60cb44
Davi P. Oliveira <dpo.dev11@gmail.com>
2025-10-21 00:05:27 -0300

    added namespaces

---
+++ 04-namespaces/namespaces.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+namespace first {
+    int x = 1;
+}
+
+namespace second {
+    int x = 2;
+}
+
+int main() {
+
+    // Namespaces = provides a solution for preventing name conflicts
+    //              in large projects. Each entity needs a unique name.
+    //              A namespace allows for indentically named entities
+    //              as long as the namespaces are different
+
+    int x = 0;
+
+    std::cout << x;
+    std::cout << first::x;
+    std::cout << second::x;
+
+    return 0;
+}
