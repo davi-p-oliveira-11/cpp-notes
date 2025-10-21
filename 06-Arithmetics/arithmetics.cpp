b0b66c58c6c2632ad7ee48b4f25eb7bf5dbd451c
Davi P. Oliveira <dpo.dev11@gmail.com>
2025-10-21 00:29:49 -0300

    added arithmetics

---
+++ 06-Arithmetics/arithmetics.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+double a,b;
+
+cout << "Enter first number: ";
+cin >> a;
+
+cout << "Enter second number: ";
+cin >> b;
+
+cout << "The sum of two numbers is: " << a + b << endl;
+cout << "The subtraction of two numbers is: " << a - b << endl;
+cout << "The multiplication of two numbers is: " << a * b << endl;
+cout << "The division of two numbers is: " << a / b << endl;
+//cout << "The remainder of two numbers is: " << a % b << endl;
+
+return 0;
+
+}
